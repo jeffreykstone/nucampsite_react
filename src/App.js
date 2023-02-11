8d2e8df178fedf4d29f976abe29312206e0b85bc
Jeff "Gent" Stone <jeffreykstone@gmail.com>
2023-02-11 12:02:14 -0800

    Updated App.js to use CampsitesList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import CampsitesList from './features/campsites/CampsitesList.js';
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      hello nucamp!
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Navbar dark color='primary' sticky='top' expand='md'>
+                <Container>
+                    <NavbarBrand href='/'>
+                        <img src={NucampLogo} alt='nucamp logo' />
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+            <CampsitesList />
+        </div>
+    );
 }
 
 export default App;
