a1b6a210f828db0bb340bc06c73f4853ced6763f
Jeff "Gent" Stone <jeffreykstone@gmail.com>
2023-02-18 05:13:44 -0800

    Added CampsitesDirectoryPage, campsitesSlice  and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import CampsitesList from './features/campsites/CampsitesList.js';
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
-import './App.css';
 import Header from './components/Header';
-
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
 
 function App() {
     return (
         <div className='App'>
             <Header />
-            <CampsitesList />
+            <CampsitesDirectoryPage />
             <Footer />
         </div>
     );
