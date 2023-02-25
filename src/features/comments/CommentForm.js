bfa1d92ef80531663c80feeb735c0ac1de6f9e7b
Jeff "Gent" Stone <jeffreykstone@gmail.com>
2023-02-25 14:59:05 -0800

    ContactForm Part 3

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,13 +1,26 @@
+import React from 'react';
 import { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateCommentForm } from '../../utils/validateCommentForm';
 
 const CommentForm = ({campsiteId}) => {
     const [modalOpen, setModalOpen] = useState(false);
 
+    const handleSubmit = (values) => {
+        const comment = {
+            campsiteId: parseInt(campsiteId),
+            rating: values.rating,
+            author: values.author,
+            text: values.commentText
+        };
+        console.log(comment);
+        setModalOpen(false);
+    };
+
     return (
         <>
-            <Button outline onClick = {() => setModalOpen(true)} >
+            <Button outline onClick={() => setModalOpen(true)} >
                 <i className='fa fa-pencil fa-lg' /> Add Comment
             </Button>
             <Modal isOpen={modalOpen}>
@@ -16,36 +29,60 @@ const CommentForm = ({campsiteId}) => {
                 </ModalHeader>
                 <ModalBody>
                     <Formik
+                        validate={validateCommentForm}
                         initialValues={{
                             rating: undefined,
                             author: '',
                             commentText: ''
                         }}
-                        onSubmit={handleSubmit()}
+                        onSubmit={handleSubmit}
                     >
                         <Form>
                             <FormGroup>
-                                <Label>Rating</Label>
-                                <Field />
-
+                                <Label htmlFor='rating'>Rating</Label>
+                                <Field
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option value='select'>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Field>
+                                <ErrorMessage name='rating'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
                             </FormGroup>
                             <FormGroup>
-                                <Label>Your Name</Label>
-                                <Field />
+                                <Label htmlFor='author'>Your Name</Label>
+                                <Field
+                                    name='author'
+                                    placeholder='Your Name'
+                                    classname='form-control'
+                                 />
+                                 <ErrorMessage name='author'>
+                                    {(msg) => <p classname='text-danger'>{msg}</p>}
+                                 </ErrorMessage>
                             </FormGroup>
                             <FormGroup>
-                                <Label>Comment</Label>
-                                <Field />
+                                <Label htmlFor='commentText'>Comment</Label>
+                                <Field 
+                                    name='commentText'
+                                    as='tetarea'
+                                    rows='12'
+                                    className='form-control'
+                                />
                             </FormGroup>
-
+                            <Button type='submit' color='primary'>Submit</Button>
                         </Form>
-                        
                     </Formik>
-                    campsite: {campsiteId}
                 </ModalBody>
             </Modal>
         </>
-    )
+    );
 };
 
 export default CommentForm;
