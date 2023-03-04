82335f96671a0f26e97c6ce233da885f8ba3882b
Jeff "Gent" Stone <jeffreykstone@gmail.com>
2023-03-04 13:55:39 -0800

    Task 1 Workshjop 4

---
+++ src/features/user/UserLoginForm.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentUser, selectCurrentUser } from './userSlice';
+import {
+    Modal,
+    ModalHeader,
+    ModalBody,
+    FormGroup,
+    Label,
+    Button
+} from 'reactstrap';
+import { Formik, Field, Form } from 'formik';
+import defaultAvatar from '../../app/assets/img/unicorn.png';
