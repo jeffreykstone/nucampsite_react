441841159e3d429eb4ba745ce9b00f44f6dd1181
Jeff "Gent" Stone <jeffreykstone@gmail.com>
2023-03-10 21:11:43 -0800

    workshop week 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import { 
-    Navbar,
+    Navbar, 
     NavbarBrand,
     Collapse,
     NavbarToggler,
-    Nav,
-    NavItem
+    Nav, 
+    NavItem,
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import NucampLogo from '../app/assets/img/logo.png';
+import UserLoginForm from '../features/user/UserLoginForm';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -17,10 +18,9 @@ const Header = () => {
         <Navbar dark color='primary' sticky='top' expand='md'>
             <NavbarBrand className='ms-5' href='/'>
                 <img src={NucampLogo} alt='nucamp logo' className='float-start' />
-                <h1 className='mt-1' >NuCamp</h1>
+                <h1 className='mt-1'>NuCamp</h1>
             </NavbarBrand>
-
-            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
             <Collapse isOpen={menuOpen} navbar>
                 <Nav className='ms-auto' navbar>
                     <NavItem>
@@ -30,23 +30,24 @@ const Header = () => {
                     </NavItem>
                     <NavItem>
                         <NavLink className='nav-link' to='/directory'>
-                            <i className='fa fa-list fa-lg' /> Directory                            
+                            <i className='fa fa-list fa-lg' /> Directory
                         </NavLink>
                     </NavItem>
                     <NavItem>
                         <NavLink className='nav-link' to='/about'>
-                            <i className='fa fa-info fa-lg' /> About                            
+                            <i className='fa fa-info fa-lg' /> About
                         </NavLink>
                     </NavItem>
                     <NavItem>
                         <NavLink className='nav-link' to='/contact'>
-                            <i className='fa fa-address-card fa-lg' /> Contact                           
+                            <i className='fa fa-address-card fa-lg' /> Contact
                         </NavLink>
                     </NavItem>
                 </Nav>
+                <UserLoginForm />
             </Collapse>
         </Navbar>
-    )
-}
+    );
+};
 
 export default Header;
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,23 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
     currentUser: null
 };
 
-const userSlice = createSlice ({
-    name: 'user',
+const userSlice = createSlice({
+    name: "user",
     initialState,
     reducers: {
         setCurrentUser: (state, action) => {
-            return (
-                {...state, currentUser: action.payload}
-            )
+            return{
+                ...state,
+                currentUser: action.payload
+            };
         }
     }
 });
 
 export const userReducer = userSlice.reducer;
-
 export const { setCurrentUser } = userSlice.actions;
-
 export const selectCurrentUser = (state) => state.user.currentUser;
+
+export default userSlice;
